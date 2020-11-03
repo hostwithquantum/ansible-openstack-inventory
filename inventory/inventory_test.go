b45d6a7421e1df79c1bf2a924077082053e2309f
till <till@php.net>
2020-11-03 11:21:30 +0100

    Chore: add basic test for inventory

---
+++ inventory/inventory_test.go
@@ -0,0 +1,47 @@
+package inventory_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hostwithquantum/ansible-openstack-inventory/inventory"
+)
+
+func ExampleNewInventory() {
+	inventory := createBasicInventory()
+	fmt.Println(inventory)
+	// Output: &{[] map[all:{all [127.0.0.1] map[] []} test:{test [127.0.0.1] map[group_var:[bar foobar]] []}] map[127.0.0.1:map[host_var:value]]}
+}
+
+func Test_ReturnJSONInventory(t *testing.T) {
+	inventory := createBasicInventory()
+	jsonB, _ := json.Marshal(inventory.ReturnJSONInventory())
+	json := string(jsonB)
+
+	tests := []string{
+		"{\"_meta\":",
+		"{\"hostvars\":{\"127.0.0.1\":{\"host_var\":\"value\"}}}",
+		"\"all\":{\"hosts\":[\"127.0.0.1\"]}",
+		"\"test\":{\"hosts\":[\"127.0.0.1\"],\"vars\":{\"group_var\":[\"bar\",\"foobar\"]}}",
+	}
+
+	for _, test := range tests {
+		ok := strings.Contains(json, test)
+		if !ok {
+			t.Log(json)
+			t.Errorf("The generated JSON doesn't contain '%s'", test)
+		}
+	}
+}
+
+func createBasicInventory() *inventory.AnsibleInventory {
+	inventory := inventory.NewInventory("customer", []string{"all", "test"})
+	inventory.AddHostToGroup("127.0.0.1", "all")
+	inventory.AddHostToGroup("127.0.0.1", "test")
+	inventory.AddHostVar("host_var", "value", "127.0.0.1")
+	inventory.AddVarToGroup("test", "group_var", []string{"bar", "foobar"})
+
+	return inventory
+}
